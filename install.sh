a351d0e5a09285acbcab8770b946a99498026293
Spidey <859511714dz@gmail.com>
2023-08-22 14:01:44 +0800

    adding some tools and vim plugins to improve the use

--- install.sh
+++ install.sh
@@ -3,29 +3,54 @@
 ########################
 # Config file for nvim #
 ########################
-mkdir -p "$HOME/.config/nvim"       # Create nvim config dir to avoid error
-mkdir -p "$HOME/.config/nvim/undo"  # Create nvim config dir to avoid erro
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"  # Create soft link to config file 
+mkdir -p "$XDG_CONFIG_HOME/nvim"       # Create nvim config dir to avoid error
+mkdir -p "$XDG_CONFIG_HOME/nvim/undo"  # Create nvim config dir to avoid erro
+ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"  # Create soft link to config file 
+
+###############################
+# Nvim Plugin Manager Install #
+###############################
+[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+# Install or upadate all the plugins
+nvim --noplugin +PlugUpdate +qa
 
 ############################
 # Config file for X server #
 ############################
-rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config"  # Create a symlink targeting the whole directory X11 because everything in this directory should be shared between systems.
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"  # Create a symlink targeting the whole directory X11 because everything in this directory should be shared between systems.
 
 #############
 # i3 config #
 #############
-rm -rf "$HOME/.config/i3"                   # delete the original config file
-ln -s "$HOME/dotfiles/i3" "$HOME/.config"   # create a symlink targeting the whole i3 directory config file
+rm -rf "$XDG_CONFIG_HOME/i3"                   # delete the original config file
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"   # create a symlink targeting the whole i3 directory config file
 
 ##############
 # zsh config #
 ##############
-mkdir -p "$HOME/.config/zsh"  # Create the zsh config file
-ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME" # Create a symlink targeting .zshenv to $HOME
-ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh" # Create a symlink targeting .zshrc to .config
-ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases" # Target aliases setting file to .config
+mkdir -p "$XDG_CONFIG_HOME/zsh"  # Create the zsh config file
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME" # Create a symlink targeting .zshenv to $HOME
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh" # Create a symlink targeting .zshrc to .config
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases" # Target aliases setting file to .config
 
 rm -rf "$HOME/.cofig/zsh/external"  
-ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"  # Target external dir to .config
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"  # Target external dir to .config
+
+###################
+# Fonts resources #
+###################
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"   # don't create symbolic link, because the font file will never be modified
+
+################
+# Dunst config #
+################
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc" 
+
+
--- nvim/init.vim
+++ nvim/init.vim
@@ -20,3 +20,28 @@ set shiftwidth=4
   
 " Set Command-Line mode history number
 set history=200
+
+" Set Key Mappings "
+" set <space> key as a leader
+nnoremap <space> <nop>
+let mapleader = "\<space>"
+nnoremap <leader>bn :bn<cr> " buffer next 
+nnoremap <leader>r :source $MYVIMRC<cr> " set <leader> + r to reload the init.vim file
+
+" Use vim-plug to manage plug:ns "
+call plug#begin("$XDG_CONFIG_HOME/nvim/plugged") " Set the plugin install path
+Plug 'chrisbra/csv.vim' " make csv file more reable
+Plug 'moll/vim-bbye' " close buffer using `Bdelete`, keep the window so it can keep the window layout
+Plug 'simeji/winresizer' " manage the windows
+Plug 'junegunn/fzf.vim', { 'do':{ -> fzf#install() } }
+Plug 'junegunn/fzf.vim' " fuzzy search in buffer list, command history, and so on
+Plug 'simnalamburt/vim-mundo' " manipulate the undo tree
+call plug#end()
+
+" Config for csv.vim
+autocmd BufRead,BufWritePost *.csv :%ArrangeColumn! "Format CSV more reable when read or write buffer
+autocmd BufWritePre *.csv :%UnArrangeColumn  "Don't save the fomatting when write the file
+
+" Config for winresizer
+let g:winresizer_start_key = "<leader>w" " User <ctrl> + w to use plugin to manage window
+
